b9468ff36766f39563065789b5956367f8137d37
messatrt <essaessa9068@gmail.com>
2024-10-19 12:03:43 +0530

    completed the config

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 -- Set the leader key to <Space>
 vim.opt.termguicolors = true
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Keymaps
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>p', ':Ex<CR>', { desc = 'Open file explorer (:Ex)' })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>p", ":Ex<CR>", { desc = "Open file explorer (:Ex)" })
 
 -- Line numbers
 vim.opt.number = true
@@ -20,13 +20,13 @@ vim.opt.smartindent = true
 
 -- Clipboard settings
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Visual enhancements
 vim.opt.breakindent = true
 vim.opt.cursorline = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Search settings
 vim.opt.ignorecase = true
@@ -44,24 +44,24 @@ vim.opt.undofile = true
 vim.opt.updatetime = 250
 
 -- Increase timeout for leader key sequences
-vim.opt.timeoutlen = 1000  -- 500ms to allow smoother key sequences
+vim.opt.timeoutlen = 1000 -- 500ms to allow smoother key sequences
 
 -- Display settings
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+--vim.opt.list = true
+--vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 --Highligth on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Lazy set up
 require("config.lazy")
-
+vim.cmd("colorscheme rose-pine-moon")
---
+++ lua/plugins/autoformat.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		-- Removed format_on_save function to disable automatic formatting
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+			-- Conform can also run multiple formatters sequentially
+			python = { "black" },
+			--
+			-- You can use 'stop_after_first' to run the first available formatter from the list
+			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
+}
